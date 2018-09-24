1f619abac0e4fafdb5262e9e371502ed104c9087
José Siapo <jsiapo@unitru.edu.pe>
2018-09-24 02:30:55 -0500

    Add comments

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ Convert function to lambda calc
 
 from ArgsAndApp import generate_argument_application
 
+# expression for example is f(x+y+z)=2*(x+3)
+expression = []
+values_argument = []
+expresion_input = input('Ingrese expresion: ')
+
+#creamos la lista expresion del string que se capturó ignorando Los espacios en blanco
+for i in expresion_input:
+    if i != ' ':
+        expression.append(i)
+
+# listas creadas de argumento y aplicacion
+(argument, application) = generate_argument_application(expression)
+
 # functions
 
+# Ver expresion original
 def original_expression():
     for i in expression:
         print(i, end='')
 
+#Ver expresion lambda
 def view_expression():
     print('(λ', end='')
     for i in argument:
@@ -19,26 +34,14 @@ def view_expression():
         print(i, end=' ')
     print(')', end='')
 
-
+#Vemos los valores ingresados de los argumentos
 def values_arguments():
     for i in argument:
         values_argument.append( input('{} -> '.format(i)))
     print('Los elementos son: {}'.format(values_argument))
 
-
-# expression for example is f(x+y+z)=2*(x+3)
-expression = []
-values_argument = []
-expresion_input = input('Ingrese expresion: ')
-
-for i in expresion_input:
-    if i != ' ':
-        expression.append(i)
-
 print('\n---------------------------------------\nConvert function to lambda '
       'calc\n---------------------------------------\n')
-
-(argument, application) = generate_argument_application(expression)
 
 print('Function')
 original_expression()
